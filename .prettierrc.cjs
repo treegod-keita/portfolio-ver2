669457dc23ae2d88d5ed1f16b869ea676c9cb9ce
榊啓太 <treegod0205@gmail.com>
2026-08-25 20:57:23 +0900

    環境構築
    - astro
    - typescript
    - tailwind
    - prittier

---
+++ .prettierrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  trailingComma: "all",
+  printWidth: 100,
+  tabWidth: 4,
+  semi: true,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+};
