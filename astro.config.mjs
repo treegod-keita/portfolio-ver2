34547b9687384e5bb82e1a652c72bb8e3a93f24f
榊啓太 <treegod0205@gmail.com>
2026-08-27 01:24:06 +0900

    feat: tailwindのセットアップ
    - colorを追加
    - text-sizeを追加
    - font-familyを追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  }
+    vite: {
+        plugins: [tailwindcss()],
+    },
+    fonts: [
+        {
+            provider: fontProviders.google(),
+            name: "Barlow", // Google Fontsでの正式名称
+            cssVariable: "--font-barlow",
+            weights: [500, 600],
+            subsets: ["latin"], // 日本語を使う場合
+        },
+    ],
 });
